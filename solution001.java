843a16da23f320122377b02b2d4ced2088982b46
Max <78800453+maxleirbag@users.noreply.github.com>
2021-07-10 19:30:13 -0300

    Optimizing: solution001
    Using mathematical series to improve the code's performance

--- solution001.java
+++ solution001.java
@@ -1,32 +1,26 @@
 package euler;
 
-import java.util.*;
-import java.io.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
 public class solution001 {
 
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("How many times do you wish to find the sum of multiples of 3 or 5?");
-		int times = input.nextInt();
-		int sum = 0, number = 0;
-
-		for (int run = 0; run < times; run++) {
-
-			System.out.println("The number of which the multiples you wish to find:");
-			number = input.nextInt();
-			for (int i = 0; i < number; i++) {
-				if (i % 3 == 0 || i % 5 == 0) {
-					sum += i;
-				}
-			}
-			System.out.println(sum);
-			sum = 0;
-		}
-		input.close();
-	}
-
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int times = input.nextInt();
+        long sum = 0, number = 0;
+                
+        for(int run = 0; run < times; run++){
+            
+            number = input.nextInt();
+            long three = (number-1) / 3;
+            long five = (number-1) / 5;
+            long fifteen = (number-1) / 15;
+            
+            sum = 3*(three*(three+1)/2)+5*(five*(five+1)/2)-15*(fifteen*(fifteen+1)/2);
+            
+            System.out.println(sum);
+            sum = 0;
+        }        
+        input.close();
+    }
 }
